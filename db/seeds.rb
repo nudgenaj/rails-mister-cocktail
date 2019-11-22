ccb64e087ab7a7d29f76d36747f5388d7aca29c8
Najihah Talib <najihahtalib@gmail.com>
2019-11-22 15:56:30 +0800

    Put up index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts 'Creating ingredients...'
+puts "Dropping the seeds"
+Cocktail.destroy_all if Rails.env.development? || Rails.new.env.test?
+Ingredient.destroy_all if Rails.env.development?|| Rails.new.env.test?
+
+puts "Creating the seeds"
+Cocktail.create(name: "Lemonade")
+Cocktail.create(name: "Spritzer")
+Cocktail.create(name: "Minty")
+
+puts "Creating the seeds"
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
-
-Ingredient.create!(ingredients_attributes)
-puts 'Finished!'
